69a21aade645b792ea3c0f607864f90116b8a0a2
Ikhsan Abdul Rafi <107069762+icanselalubenar@users.noreply.github.com>
2023-04-22 15:25:22 +0700

    Add files via upload

---
+++ Mahasiswa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Encapsulation
+{
+    internal class Mahasiswa
+    {
+        public static int jumlahMahasiswa = 0;
+        public string nama;
+        public string nim;
+        public int umur;
+
+        public Mahasiswa(string a, string b, int c)
+        {
+            this.nama = a;
+            this.nim = b;
+            this.umur = c;
+            jumlahMahasiswa++;
+        }
+
+        public static int totalMhs
+        {
+            get { return jumlahMahasiswa; }
+            set { jumlahMahasiswa = value; }
+        }
+        public string namaMhs
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public string nimMhs
+        {
+            get { return nim; }
+            set { nim = value; }
+        }
+        public int umurMhs
+        {
+            get { return umur; }
+            set { umur = value; }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mahasiswa01 = new Mahasiswa("Ikhsan", "22.11.4824", 18);
+            Mahasiswa mahasiswa02 = new Mahasiswa("Iqbal", "22.11.4850", 20);
+            Mahasiswa mahasiswa03 = new Mahasiswa("Naji", "22.11.4818", 19);
+
+            int totalMahasiswa = Mahasiswa.totalMhs;
+
+            Console.WriteLine("Total Mahasiwa : {0}", totalMahasiswa);
+            Console.WriteLine("+-----------------------------------------------------------------+");
+
+            Console.WriteLine("Mahasiswa 1 : {0} dengan nim {1}, berusia {2} tahun.", mahasiswa01.namaMhs, mahasiswa01.nimMhs, mahasiswa01.umurMhs);
+            Console.WriteLine("Mahasiswa 2 : {0} dengan nim {1}, berusia {2} tahun.", mahasiswa02.namaMhs, mahasiswa02.nimMhs, mahasiswa02.umurMhs);
+            Console.WriteLine("Mahasiswa 3 : {0} dengan nim {1}, berusia {2} tahun.", mahasiswa03.namaMhs, mahasiswa03.nimMhs, mahasiswa03.umurMhs);
+
+            Console.ReadKey();
+        }
+    }
+}
